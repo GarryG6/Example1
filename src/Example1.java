d537ba3981e267e63ebbfb4df58e9e0fa83f1dba
GarryG6 <ingold333@mail.ru>
2019-06-09 22:41:36 +0800

    + Example1 Class

---
+++ src/Example1.java
@@ -0,0 +1,8 @@
+/* Это простой пример программы на Java*/
+
+class Example1 {
+    // Выполнение программы на Java начинается с вызова метода main()
+    public static void main(String[] args){
+        System.out.println("Java");
+    }
+}
